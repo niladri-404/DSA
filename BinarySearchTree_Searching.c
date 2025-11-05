7a3aa3e5d64a710091953b3bd8681ef05b171740
NIladri_404! <niladrisana12@gmail.com>
2025-11-05 14:48:22 +0530

    Add files via upload

---
+++ BinarySearchTree_Searching.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <malloc.h>
+
+struct node
+{
+    int data;
+    struct node *left;
+    struct node *right;
+};
+
+struct node *createNode(int data)
+{
+    struct node *n;
+    n = (struct node *)malloc(sizeof(struct node));
+    n->data = data;
+    n->left = NULL;
+    n->right = NULL;
+    return n;
+}
+
+int isBST(struct node *root)
+{
+    static struct node *prev = NULL;
+    if (root != NULL)
+    {
+        if (!isBST(root->left))
+            return 0;
+        if (prev != NULL && root->data <= prev->data)
+            return 0;
+        prev = root;
+        return isBST(root->right);
+    }
+    else
+        return 1;
+}
+struct node *search(struct node *root, int key)
+{
+    if (root == NULL)
+    {
+        return NULL;
+    }
+    if (key == root->data)
+    {
+        return root;
+    }
+    else if (key < root->data)
+    {
+        return search(root->left, key);
+    }
+    else // (key>root->data)
+    {
+        return search(root->right, key);
+    }
+}
+
+int main()
+{
+    // constructing the root node
+    struct node *p = createNode(50);
+    struct node *p1 = createNode(30);
+    struct node *p2 = createNode(70);
+    struct node *p3 = createNode(20);
+    struct node *p4 = createNode(40);
+    struct node *p5 = createNode(60);
+    struct node *p6 = createNode(80);
+
+    //       50
+    //      /  \
+    //    30    70
+    //   / \    / \
+    // 20  40  60  80
+
+    // Linking the node with left and right children
+    p->left = p1;
+    p->right = p2;
+    p1->left = p3;
+    p1->right = p4;
+    p2->left = p5;
+    p2->right = p6;
+
+    if (isBST(p))
+    {
+        printf("\nThis is a BST.\n");
+    }
+
+    struct node *n = search(p, 70);
+    if (n != NULL)
+    {
+        printf("Found %d", n->data);
+    }
+
+    return 0;
+}
